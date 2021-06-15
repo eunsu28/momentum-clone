250568fa3a09d17e16e16ae62fdadfbdb275983e
eunsu28 <eunsuvg@gmail.com>
2021-06-15 22:06:14 +0800

    6/15 geo b

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const a = 8;
-const b = 10;
-const veryLongVariableName = "Ayden";
+let a = 8;
+let b = 10;
+let name = "Ayden";
 
-console.log(a + b)
-console.log(veryLongVariableName + " hello")
+console.log(a + b);
+console.log(name + " hello");
+
+name = "EUN SU";
+
+console.log(name);
+
+// constant can not change (상수)
+// variable with "let" can change value (변수)
+// Before time we use "var" <- 옛날 거 아무것도 구분 못 함
+// let 은 새로운 것을 생성 할 때만  
