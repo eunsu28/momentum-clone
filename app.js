af4d1f59bf3ab6dc40d310a21542aa55421364fc
eunsu28 <eunsuvg@gmail.com>
2021-06-20 18:16:51 +0800

    hello

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-let a = 8;
-let b = 10;
-let name = "Ayden";
-
-console.log(a + b);
-console.log(name + " hello");
-
-name = "EUN SU";
-
-console.log(name);
+const amIsmart = null;
+let a;
+console.log(amIsmart)
+console.log(a )
 
 // constant can not change (상수)
 // variable with "let" can change value (변수)
 // Before time we use "var" <- 옛날 거 아무것도 구분 못 함
 // let 은 새로운 것을 생성 할 때만  
+// boolean은 true 또는 False
+// "null" mean none, nothing
+// "undefined"는 정의 되지 않았다 
