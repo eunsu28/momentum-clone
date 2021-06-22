4c70ddb2733c3bd8f66bd29859255b58f7a46a16
eunsu28 <eunsuvg@gmail.com>
2021-06-22 20:18:28 +0800

    hello

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const a = ["a", "b", "c"];
-console.log(a)
+const aboutAy = {
+    name = "Ayden",
+    age = 14,
+    favoriteSport = "baseball",
+    favoriteFood = "Kimchi",
+    areHeFat = false,
+}
+
+alert(aboutAy.name);
 
 // constant can not change (상수)
 // variable with "let" can change value (변수)
@@ -9,3 +16,4 @@ console.log(a)
 // "null" mean none, nothing
 // "undefined"는 정의 되지 않았다 
 // array 는 파이썬에 리스트와 비슷 하다, 그러나 데이터와 더 가깝다, index도 똑같이 적용된다
+// object 를 만들기 위해서는 {}를 사용한다
