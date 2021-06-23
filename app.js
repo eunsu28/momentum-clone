cc59728ba74a7b7958977288ebb649f11ef16c45
eunsu28 <eunsuvg@gmail.com>
2021-06-23 21:17:04 +0800

    hello

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const player = {
-    name : "Ayden",
-    age : 14,
-    favoriteSport : "baseball",
-    favoriteFood : "Kimchi",
-    areHeFat : false,
-};
+function sayHello() {
+    alert("hello");
+}
+sayHello()
 
-alert(player.name);
+
 
 // constant can not change (상수)
 // variable with "let" can change value (변수)
@@ -16,5 +13,6 @@ alert(player.name);
 // "null" mean none, nothing
 // "undefined"는 정의 되지 않았다 
 // array 는 파이썬에 리스트와 비슷 하다, 그러나 데이터와 더 가깝다, index도 똑같이 적용된다
-// object 를 만들기 위해서는 {}를 사용한다
+// object 를 만들기 위해서는 {}를 사용한다, 파이썬의 딕션어리와 비슷하다
 // 수정, 추가도 가능, ex: ayden.siblings = true;
+// function을 만들기 위해서는 function을 적는다, 오브젝트와 비슷
