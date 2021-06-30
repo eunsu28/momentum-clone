4bd0b512464fa48209aba9e4a7c866ffe9fd12a5
eunsu28 <eunsuvg@gmail.com>
2021-06-30 08:56:38 +0800

    hololo

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const player = {
-    name : "ay",
-    sayHello : function(name){
-        console.log("hello " + name);
+const calculator = {
+    add : function(a, b) {
+        console.log(a + b)
     },
+
+    minus : function(a, b) {
+        console.log(a - b)
+    },
+
+    multiple : function(a, b) {
+        console.log(a * b)
+    },
+
+    divide : function(a, b) {
+        console.log(a / b)
+    }, 
 };
 
-player.sayHello();
-
+console.log(calculator.add(2, 1))
+console.log(calculator.minus(2, 1))
+console.log(calculator.multiple(2, 1))
+console.log(calculator.divide(2, 1))
 // constant can not change (상수)
 // variable with "let" can change value (변수)
 // Before time we use "var" <- 옛날 거 아무것도 구분 못 함
