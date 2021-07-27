3d0d2913e22fb06b1e43d852ff6c7f0745d103f3
eunsu28 <ayay.e2008@gmail.com>
2021-07-27 20:43:38 +0800

    df

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const hello = document.querySelector(".hello h1 ")
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log(hello)
+function titleClick(){
+    console.log("title was clicked");
+    title.style.color = "red";
+}
+
+title.addEventListener("click", titleClick);
 
 
 // querySelector css 방식. 멘첫번째 꺼만 찾음 
+// hello.style.color = "blue";
+//function titleClick(){
+//     console.log("title was clicked");
+// }
+
+// hello.addEventListener("click", titleClick);
