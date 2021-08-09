a0daf776d0f733e943d8ab0a701b1467f4bea26f
eunsu28 <ayay.e2008@gmail.com>
2021-08-09 20:36:53 +0800

    Df

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const title = document.querySelector("div.hello:first-child h1");
-let click = 0;
 
 function titleClick(){
-    const cColor = title.style.color
-    let nColor;
-    if(cColor === "blue") {
-        nColor = "tomato";
-    } else{
-        nColor = "blue";
+    const clickedClass = "clicked";
+    if (title.className === clickedClass) {
+        title.className = "";
+    } else {
+        title.className = clickedClass;
     }
-    title.style.color = nColor
-    
-}
+} 
 
 
 title.addEventListener("click", titleClick); 
