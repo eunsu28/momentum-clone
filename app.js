67785208f09c458c85505ebe08dce7772feea4aa
eunsu28 <ayay.e2008@gmail.com>
2021-08-13 20:53:10 +0800

    dfd

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const loginInput = document.querySelector("#login input");
 const loginButton = document.querySelector("#login button");
 
 function buttonClick() {
-    console.log("Hello", loginInput.value);
-    console.log("clicked");
+    const username = loginInput.value;
+    console.log(username)
 }
 
 loginButton.addEventListener("click", buttonClick)
+
+//.length tell the length 
