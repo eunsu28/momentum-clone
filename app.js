06de21c18414bce3f83d7fdc2f2f38a6a2bd2373
eunsu28 <ayay.e2008@gmail.com>
2021-08-14 17:04:34 +0800

    8/13.5

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+const loginForm = document.querySelector("#login");
 const loginInput = document.querySelector("#login input");
-const loginButton = document.querySelector("#login button");
 
-function buttonClick() {
+
+function onLoginSubmit() {
     const username = loginInput.value;
-    console.log(username)
+    console.log(username);
 }
 
-loginButton.addEventListener("click", buttonClick)
+loginForm.addEventListener("submit", onLoginSubmit);
 
 //.length tell the length 
