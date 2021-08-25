63c9ad023ad8db5dc68c32e4d9bea38b4cde2a2e
eunsu28 <ayay.e2008@gmail.com>
2021-08-25 21:01:14 +0800

    timeout

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,9 @@
 const clock = document.querySelector("h2#clock");
 
+function sayHello() {
+    console.log("Hello");
+}
+
+setTimeout(sayHello, 1000)
 // setInterval(function, milli second)
+// set Timeout
