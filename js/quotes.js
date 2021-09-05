46ee0846f25d06f063385b284745090044e6b3d1
eunsu28 <ayay.e2008@gmail.com>
2021-09-05 18:43:21 +0800

    error fixed & innerText to show quote

--- js/quotes.js
+++ js/quotes.js
@@ -40,4 +40,7 @@ const quotes = [
 const quote = document.querySelector("#quote span:first-child")
 const author = document.querySelector("#quote span:last-child")
 
-const todaysQutoe = console.log(quotes[Math.floor(Math.random() * quotes.length)])
+const todaysQutoe = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQutoe.quote;
+author.innerText = todaysQutoe.author;
