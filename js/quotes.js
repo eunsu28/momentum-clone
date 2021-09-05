a97aaa1c54c0af424990e34bc81bf865cb26857e
eunsu28 <ayay.e2008@gmail.com>
2021-09-05 12:25:25 +0800

    add quotes

---
+++ js/quotes.js
@@ -0,0 +1,38 @@
+const quotes = [
+    {
+        quote: "피할수 없으면 즐겨라",
+        author: "로버트 엘리엇",
+    },
+    {
+        quote: "평생 살 것처럼 꿈을 꾸어라.그리고 내일 죽을 것처럼 오늘을 살아라",
+        author: "제임스 딘",
+    },
+    {
+        quote: "You are the world and the world is you.",
+        author: "KRISHNAMURTI",
+    },
+    {
+        quote: "Be yourself; everyone else is already taken.",
+        author: "Oscar Wilde",
+    },
+    {
+        quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        author: "Albert Einstein",
+    },
+    {
+        quote: "A room without books is like a body without a soul.",
+        author: "Marcus Tullius Cicero",
+    },
+    {
+        quote: "화가 날 때는 100까지 세라. 최악일 때는 욕설을 퍼부어라",
+        author: "마크 트웨인",
+    },
+    {
+        quote: "작은 기회로 부터 종종 위대한 업적이 시작된다",
+        author: "데모스테네스",
+    },
+    {
+        quote: "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
+        author: "Martin Luther King Jr",
+    },
+]
