582cae4fae4e4e71ed39ad8c24781c44d808d916
eunsu28 <ayay.e2008@gmail.com>
2021-09-24 19:42:22 +0800

    comments background.js / clock.js

--- js/background.js
+++ js/background.js
@@ -1,9 +1,19 @@
+// background image arrary
 const images = ["1.png", "2.jpg", "3.jpg", "4.jpg"];
+//
 
+// randomly choose image
 const chosenImage = images[Math.floor(Math.random() * images.length)];
+//
 
+//create img tag on HTML
 const beginImg = document.createElement("img");
+//
 
+// apply image num
 beginImg.src = `img/${chosenImage}`;
+//
 
+//apply to HTML
 document.body.appendChild(beginImg);
+//
--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,8 @@
+// querySelector
 const clock = document.querySelector("h2#clock");
+//
 
+// get time using data.getHours / getMInutes / getSeconds
 function getTime() {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2, "0");
@@ -7,7 +10,9 @@ function getTime() {
     const seconds = String(date.getSeconds()).padStart(2, "0");
     clock.innerText = (`${hours}:${minutes}:${seconds}`);
 }
+//
 
+//start
 getTime();
 setInterval(getTime, 1000);
 // setInterval(function, milli second)
