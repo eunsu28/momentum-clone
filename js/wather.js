6ede0d4dc3e6fbf484e56ea4d1daf35e1e183b91
eunsu28 <ayay.e2008@gmail.com>
2021-10-10 17:14:38 +0800

    weather api

--- js/wather.js
+++ js/wather.js
@@ -17,4 +17,6 @@ function onGeoError() {
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError)
 //
 
+//api.openweathermap.org/data/2.5/weather?lat={31.230416}&lon={121.473701}&appid={f2f5d900c5a5a9ba2f13a65a8df7ec6d}
+
 //using navigator.geolocation.getCurrentPosition()
